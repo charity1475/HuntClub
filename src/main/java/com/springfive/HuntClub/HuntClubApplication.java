272f0cb35a1ae965cf71702066e9e21bbd082566
Steinny <charitymbisi@gmail.com>
2020-11-01 22:00:26 +0300

    starting

---
+++ src/main/java/com/springfive/HuntClub/HuntClubApplication.java
@@ -0,0 +1,13 @@
+package com.springfive.HuntClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntClubApplication.class, args);
+	}
+
+}
