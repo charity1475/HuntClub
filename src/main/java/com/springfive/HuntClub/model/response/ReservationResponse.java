0d2a2da1df60455bebefa6adc416fe69291e4c71
Steinny <charitymbisi@gmail.com>
2020-11-01 23:57:03 +0300

    build tool

--- src/main/java/com/springfive/HuntClub/model/response/ReservationResponse.java
+++ src/main/java/com/springfive/HuntClub/model/response/ReservationResponse.java
@@ -1,6 +1,10 @@
 package com.springfive.HuntClub.model.response;
 
+import lombok.Data;
+@Data
 public class ReservationResponse {
     private Long id;
-    
+    private Integer roomNumber;
+    private Integer price;
+    private Links links;
 }
