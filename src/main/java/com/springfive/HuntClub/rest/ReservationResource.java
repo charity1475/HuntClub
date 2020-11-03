197d4e1688cc631753ad5ad6f57b74e1d489bf07
Steinny <charitymbisi@gmail.com>
2020-11-03 12:45:57 +0300

    create reservation

--- src/main/java/com/springfive/HuntClub/rest/ReservationResource.java
+++ src/main/java/com/springfive/HuntClub/rest/ReservationResource.java
@@ -5,10 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -25,5 +22,13 @@ public class ReservationResource {
             LocalDate checkout) {
 
                 return  new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+    @RequestMapping(path = "", method =RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+    consumes = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody
+            ReservationRequest reservationRequest){
+            return new ResponseEntity<>(new ReservationResponse(),HttpStatus.CREATED);
     }
 }
