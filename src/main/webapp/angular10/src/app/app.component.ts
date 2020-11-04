b3dbfc66ab22dd416ba4800a4e46fb7349b72486
Steinny <charitymbisi@gmail.com>
2020-11-05 00:14:41 +0300

    new

--- src/main/webapp/angular10/src/app/app.component.ts
+++ src/main/webapp/angular10/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', "../node_modules/bootstrap/dist/css/bootstrap.css"]
 })
-export class AppComponent {
-  title = 'angular10';
+export class AppComponent implements OnInit{
+
+  public submitted:boolean;
+  roomSearch: FormGroup;
+  title: null;
+  ngOnInit() {
+    this.roomSearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+  onSubmit({value,valid}:{value:Roomsearch,valid:boolean}){
+      console.log(value);
+  }
+}
+export interface Roomsearch{
+  checkin:string;
+  checkout:string;
 }
--- src/main/webapp/angular10/src/main.ts
+++ src/main/webapp/angular10/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 if (environment.production) {
   enableProdMode();
